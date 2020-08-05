f9faa811fe5657d582ec06136be0b1bfbd8676ce
Ramiz Hanan <41353957+RamizHanan@users.noreply.github.com>
2020-08-04 21:51:52 -0700

    Implemented Joystick control using Gamepad API and created Joy ROS Publisher. Need to test published data.

--- smile_mobile_robot_ws/src/smile_mobile_web/src/App.vue
+++ smile_mobile_robot_ws/src/smile_mobile_web/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      clipped-left
-    >
-      
+    <v-app-bar app clipped-left>
       <v-toolbar-title>SMILE Robotics</v-toolbar-title>
     </v-app-bar>
 
@@ -12,14 +8,33 @@
       <v-container fluid>
         <v-row>
           <v-col>
-            <OdometryDisplay v-bind:roll="roll" v-bind:pitch="pitch"  v-bind:yaw="yaw" />
+            <OdometryDisplay
+              v-bind:roll="roll"
+              v-bind:pitch="pitch"
+              v-bind:yaw="yaw"
+            />
           </v-col>
           <v-col>
-            <img :src="camera"/>
+            <img :src="camera" width="600" height="400" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="text-center">
+            <v-btn
+              large
+              v-on:click="toggleMotorState"
+              color="error"
+              v-if="motorsEnabled"
+              >Disable Motors</v-btn
+            >
+            <v-btn large v-on:click="toggleMotorState" color="success" v-else
+              >Enable Motors</v-btn
+            >
           </v-col>
         </v-row>
       </v-container>
-      <WASDTeleop v-on:wasdCommand="receiveWASD"/>
+      <WASDTeleop v-on:wasdCommand="receiveWASD" />
+      <Joystick v-on:joyCommand="receiveJOY" />
     </v-main>
 
     <v-footer app>
@@ -28,60 +43,69 @@
   </v-app>
 </template>
 
-
-
 <script>
-  import OdometryDisplay from '@/components/OdometryDisplay'
-  import ros_mixin from './ros_mixin.js'
-  import WASDTeleop from '@/components/WASDTeleop/WASDTeleop'
+import OdometryDisplay from "@/components/OdometryDisplay";
+import ros_mixin from "./ros_mixin.js";
+import WASDTeleop from "@/components/WASDTeleop/WASDTeleop";
+import Joystick from "@/components/Joystick/Joystick";
 
-  export default {
-    props: {
-      source: String,
-    },
+export default {
+  props: {
+    source: String,
+  },
 
-    /*
+  /*
     ros_mixin contains the connections with getting and receiving information
     from ros.
     */
-    mixins: [ros_mixin],
+  mixins: [ros_mixin],
 
-    components: {
-      OdometryDisplay,
-      WASDTeleop,
-    },
+  components: {
+    OdometryDisplay,
+    WASDTeleop,
+    Joystick,
+  },
 
-    methods : {
-      receiveWASD : function(W, A, S, D){
-        console.log("-----------------------");
+  methods: {
+    receiveWASD: function(W, A, S, D) {
+      console.log("-----------------------");
+      this.left_pwm_raw = W * 50 + S * -50 + A * -40 + D * 40;
+      this.right_pwm_raw = W * 50 + S * -50 + A * 40 + D * -40;
+      this.left_pwm.shift();
+      this.left_pwm.push(this.left_pwm_raw);
+      this.right_pwm.shift();
+      this.right_pwm.push(this.right_pwm_raw);
 
-        this.left_pwm_raw = (W * 50) + (S * -50) + (A * -40) + (D * 40);
-        this.right_pwm_raw = (W * 50) + (S * -50) + (A * 40) + (D * -40);
-
-        this.left_pwm.shift(); this.left_pwm.push(this.left_pwm_raw);
-        this.right_pwm.shift(); this.right_pwm.push(this.right_pwm_raw);
-
-        
-        let left_sum = 0; let right_sum = 0;
-        for(let i = 0; i < 10; i++) {
-          left_sum += this.left_pwm[i]; right_sum += this.right_pwm[i];
-
-        }
-        left_sum = left_sum / 10.0; right_sum = right_sum / 10.0;
-        
-        this.pwm_message.data = [left_sum, right_sum, right_sum, left_sum]
-        console.log(this.pwm_message.data);
-        this.pwm_topic.publish(this.pwm_message);
+      let left_sum = 0;
+      let right_sum = 0;
+      for (let i = 0; i < 10; i++) {
+        left_sum += this.left_pwm[i];
+        right_sum += this.right_pwm[i];
       }
-    },
+      left_sum = left_sum / 10.0;
+      right_sum = right_sum / 10.0;
 
-    created () {
-      this.$vuetify.theme.dark = true;
+      this.pwm_message.data = [left_sum, right_sum, right_sum, left_sum];
+      console.log(this.pwm_message.data);
+      this.pwm_topic.publish(this.pwm_message);
+    },
+    receiveJOY: function(buttonValue,axes) {
+
+      this.joy_message.buttons = buttonValue;
+      this.joy_message.axes = axes;
       
+      console.log(this.joy_message.buttons);
+      console.log(this.joy_message.axes);
+      this.joy_publisher.publish(this.joy_message);
     },
+  },
 
-    mounted () {
-      this.connect('ws://192.168.1.107:9090');
-    },
-  }
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+
+  mounted() {
+    this.connect("ws://192.168.1.107:9090");
+  },
+};
 </script>
--- smile_mobile_robot_ws/src/smile_mobile_web/src/ros_mixin.js
+++ smile_mobile_robot_ws/src/smile_mobile_web/src/ros_mixin.js
@@ -1,92 +1,131 @@
-import ROSLIB from 'roslib'; //Library providing all the connections with ROS.
+import ROSLIB from "roslib"; //Library providing all the connections with ROS.
 
 export default {
+  data: function() {
+    return {
+      roll: "N/A",
+      pitch: "N/A",
+      yaw: "N/A",
+      drawer: null,
+      ros: null,
+      connected: false, //Are we connected to robot?
+      camera: "./assets/logo.png",
+      imu_subscriber: null, //Only connect when successful connection is established
+      image_topic: null,
+      pwm_topic: null,
+      pwm_message: null,
+      joy_publisher: null,
+      joy_topic: null,
+      joy_message: null,
+      left_pwm_raw: 0,
+      right_pwm_raw: 0,
+      left_pwm: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      right_pwm: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-    data : function() {
-        return{
-        
-            roll : "N/A",
-            pitch : "N/A",
-            yaw : "N/A",
-            drawer: null,
-            ros : null,
-            connected : false, //Are we connected to robot?
-            camera : './assets/logo.png',
-            imu_subscriber : null, //Only connect when successful connection is established
-            image_topic : null,
-            pwm_topic : null,
-            pwm_message : null,
-            left_pwm_raw : 0,
-            right_pwm_raw : 0,
-            left_pwm : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            right_pwm : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          }
+      motorsEnabled: true,
+      motors_enable_request_message: null,
+      motors_enable_service: null,
+    };
+  },
+
+  methods: {
+    //Connect to a ROS websocket to communicate with selected vehicle
+    connect: function(url) {
+      this.ros = new ROSLIB.Ros({
+        url: url,
+      });
+
+      this.ros.on("connection", function() {
+        console.log("Connected to websocket server.");
+        this.connected = true;
+      });
+
+      console.log(this.connected);
+      this.ros.on("error", function(error) {
+        console.log("Error connecting to websocket server: ", error);
+        this.connected = false;
+      });
+
+      this.ros.on("close", function() {
+        console.log("Connection to websocket server closed.");
+        this.connected = false;
+      });
+
+      //Connect to the IMU data (Odometry)
+      this.imu_subscriber = new ROSLIB.Topic({
+        ros: this.ros,
+        name: "/smile/imu",
+        messageType: "sensor_msgs/Imu",
+      });
+
+      this.imu_subscriber.subscribe(this.imu_subscriber_callback);
+
+      this.joy_publisher = new ROSLIB.Topic({
+        ros: this.ros,
+        name: "/smile/joy",
+        messageType: "sensor_msgs/Joy",
+      });
+
+      this.joy_message = new ROSLIB.Message({axes : [], buttons : []});
+      //
+      this.image_topic = new ROSLIB.Topic({
+        ros: this.ros,
+        name: "/smile/cv2_capture",
+        messageType: "sensor_msgs/CompressedImage",
+      });
+
+      this.image_topic.subscribe(this.image_topic_callback);
+
+      this.pwm_topic = new ROSLIB.Topic({
+        ros: this.ros,
+        name: "/smile/pwm",
+        messageType: "std_msgs/Int16MultiArray",
+      });
+
+      this.pwm_message = new ROSLIB.Message({
+        data: [1, 2, 3, 4],
+      });
+
+      this.motors_enable_service = new ROSLIB.Service({
+        ros: this.ros,
+        name: "/smile/motors_enabled",
+        serviceType: "std_srvs/SetBool",
+      });
+
+      this.motors_enable_request_message = new ROSLIB.ServiceRequest({
+        data: true,
+      });
     },
 
-    methods : {
-        //Connect to a ROS websocket to communicate with selected vehicle
-        connect : function(url) {
-            this.ros = new ROSLIB.Ros({
-                url: url
-            });
-
-            this.ros.on('connection', function() {
-                console.log('Connected to websocket server.');
-                this.connected = true;
-            });
-
-            console.log(this.connected);
-            this.ros.on('error', function(error) {
-                console.log('Error connecting to websocket server: ', error);
-                this.connected = false;
-            });
-
-            this.ros.on('close', function() {
-                console.log('Connection to websocket server closed.');
-                this.connected = false;
-            });  
-            
-            //Connect to the IMU data (Odometry)
-            this.imu_subscriber = new ROSLIB.Topic({
-                ros : this.ros,
-                name : '/smile/imu',
-                messageType : 'sensor_msgs/Imu'
-            });
-
-            this.imu_subscriber.subscribe(this.imu_subscriber_callback);
-
-            //
-            this.image_topic = new ROSLIB.Topic({
-            ros: this.ros,
-            name: '/smile/cv2_capture',
-            messageType: 'sensor_msgs/CompressedImage'
-            });
-
-            this.image_topic.subscribe(this.image_topic_callback);
-            
-            this.pwm_topic = new ROSLIB.Topic({
-                ros : this.ros,
-                name : '/smile/pwm',
-                messageType : 'std_msgs/Int16MultiArray',
-            });
-
-            this.pwm_message = new ROSLIB.Message({
-                data: [1, 2, 3, 4],
-            })
-            
-        },
-        
-        imu_subscriber_callback : function(message){
-        this.roll = (message.orientation.x * (180.0 / Math.PI)).toFixed(2).toString();
-        this.pitch = (message.orientation.y * (180.0 / Math.PI)).toFixed(2).toString();
-        this.yaw = (message.orientation.z * (180.0 / Math.PI)).toFixed(2).toString();
-        },
-
-        image_topic_callback : function(message){
-        this.camera = "data:image/jpg;base64," + message.data;
-        },
-
-
-        
+    imu_subscriber_callback: function(message) {
+      this.roll = (message.orientation.x * (180.0 / Math.PI))
+        .toFixed(2)
+        .toString();
+      this.pitch = (message.orientation.y * (180.0 / Math.PI))
+        .toFixed(2)
+        .toString();
+      this.yaw = (message.orientation.z * (180.0 / Math.PI))
+        .toFixed(2)
+        .toString();
     },
-}
+
+    image_topic_callback: function(message) {
+      this.camera = "data:image/jpg;base64," + message.data;
+    },
+
+    motor_enable_service_response_callback: function(response) {
+      console.log(response);
+    },
+
+    toggleMotorState: function() {
+      this.motorsEnabled = !this.motorsEnabled;
+      this.motors_enable_request_message.data = this.motorsEnabled;
+      this.motors_enable_service.callService(
+        this.motors_enable_request_message,
+        this.motor_enable_service_response_callback
+      );
+
+      console.log("Setting Motor State to " + this.motorsEnabled);
+    },
+  },
+};
