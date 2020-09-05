6a800b8c0b807d2a91710a6a38fc0fca09b1018b
Ramiz Hanan <41353957+RamizHanan@users.noreply.github.com>
2020-09-04 17:55:20 -0700

    Added web documentation for overview. Also fixed component folder structure for Odometry

--- smile_mobile_robot_ws/src/smile_mobile_web/src/components/WASDTeleop/WASDTeleop.vue
+++ smile_mobile_robot_ws/src/smile_mobile_web/src/components/WASDTeleop/WASDTeleop.vue
@@ -1,68 +1,64 @@
 <template>
-    <div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-    export default{
-        name: 'WASDTeleop',
-        props: {
-        },
-        data : function() {
-            return{
-                W : false,
-                A : false,
-                S : false,
-                D : false,
-            }
-        },
-        
-        methods : {
-            onKeyDown : function(key) {
-                if(key.keyCode === 87 /*w*/ && !(this.S)) {
-                    this.W = true;
-                }
-                else if(key.keyCode === 83 /*s*/ && !(this.W)) {
-                    this.S = true;
-                }
-                if(key.keyCode === 65 /*a*/ && !(this.D)) {
-                    this.A = true;
-                }
-                else if(key.keyCode === 68 /*d*/ && !(this.A)) {
-                    this.D = true;
-                }
-                //this.$emit('wasdCommand', this.W, this.A, this.S, this.D)
-            },
-            onKeyUp : function(key) {
-                if(key.keyCode === 87 /*w*/) {
-                    this.W = false;
-                }
-                if(key.keyCode === 83 /*s*/) {
-                    this.S = false;
-                }
-                if(key.keyCode === 65 /*a*/) {
-                    this.A = false;
-                }
-                if(key.keyCode === 68 /*d*/) {
-                    this.D = false;
-                }
-                //this.$emit('wasdCommand', this.W, this.A, this.S, this.D)
-            },
-            //Continously sends the state of the W,A,S,D command.
-            repeatedUpdate : function(){
-                setInterval(() => {
-                    this.$emit('wasdCommand', this.W, this.A, this.S, this.D);
-                }, 100)
-            }
-        }, 
-        mounted() {
-            document.addEventListener('keydown', this.onKeyDown)
-            document.addEventListener('keyup', this.onKeyUp)
-            //this.repeatedUpdate();
-        },
-        beforeDestroy() {
-            document.removeEventListener('keydown', this.onKeyDown)
-            document.removeEventListener('keyup', this.onKeyUp)
-        }
-    }
+export default {
+  name: "WASDTeleop",
+  props: {},
+  data: function() {
+    return {
+      W: false,
+      A: false,
+      S: false,
+      D: false,
+    };
+  },
+
+  methods: {
+    onKeyDown: function(key) {
+      if (key.keyCode === 87 /*w*/ && !this.S) {
+        this.W = true;
+      } else if (key.keyCode === 83 /*s*/ && !this.W) {
+        this.S = true;
+      }
+      if (key.keyCode === 65 /*a*/ && !this.D) {
+        this.A = true;
+      } else if (key.keyCode === 68 /*d*/ && !this.A) {
+        this.D = true;
+      }
+      //this.$emit('wasdCommand', this.W, this.A, this.S, this.D)
+    },
+    onKeyUp: function(key) {
+      if (key.keyCode === 87 /*w*/) {
+        this.W = false;
+      }
+      if (key.keyCode === 83 /*s*/) {
+        this.S = false;
+      }
+      if (key.keyCode === 65 /*a*/) {
+        this.A = false;
+      }
+      if (key.keyCode === 68 /*d*/) {
+        this.D = false;
+      }
+      //this.$emit('wasdCommand', this.W, this.A, this.S, this.D)
+    },
+    //Continously sends the state of the W,A,S,D command.
+    repeatedUpdate: function() {
+      setInterval(() => {
+        this.$emit("wasdCommand", this.W, this.A, this.S, this.D);
+      }, 100);
+    },
+  },
+  mounted() {
+    document.addEventListener("keydown", this.onKeyDown);
+    document.addEventListener("keyup", this.onKeyUp);
+    //this.repeatedUpdate();
+  },
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.onKeyDown);
+    document.removeEventListener("keyup", this.onKeyUp);
+  },
+};
 </script>
